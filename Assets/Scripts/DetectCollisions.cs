8c517226b72bcbd83394cb7b9f6540b24f7f556c
khushicomp <khushiyuwanathe8@gmail.com>
2025-06-22 10:09:10 +0530

    Initial commit - Unity game

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Add additional logic here, such as destroying the object or updating a score
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        }
+}
